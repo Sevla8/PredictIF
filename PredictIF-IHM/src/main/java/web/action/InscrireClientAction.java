a5d10c6465d48330ca7b534e11260848cc7e131e
Algourdin Benoit <benoit.algourdin@insa-lyon.fr>
2021-05-17 15:44:13 +0000

    Upload New File

---
+++ PredictIF-IHM/src/main/java/web/action/InscrireClientAction.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package action;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import metier.modele.Client;
+import metier.service.Service;
+
+/**
+ *
+ * @author Benoit
+ */
+public class InscrireClientAction extends Action{
+
+    @Override
+    public void executer(HttpServletRequest request) {
+        System.out.println("inscription en cours");
+        String nom = request.getParameter ("nom");
+        System.out.println("nom="+nom);
+
+        String prenom = request.getParameter ("prenom") ;
+        System.out.println("prenom="+prenom);
+        
+        String adresse = request.getParameter ("adresse") ;
+        System.out.println("adresse="+adresse);
+        
+        String tel = request.getParameter ("telephone") ;
+        System.out.println("tel="+tel);
+
+        String dateNaissanceRecue=request.getParameter ("dateDeNaissance") ;
+        Date dateDeNaissance=new Date();
+        try{
+            dateDeNaissance =new SimpleDateFormat("yyyy-MM-dd").parse(dateNaissanceRecue);
+
+            System.out.println("dateDeNaissance="+dateDeNaissance);
+        }catch(Exception e){
+            e.printStackTrace();
+            dateDeNaissance=null;
+        }
+
+        String mail = request.getParameter ("mail");
+        System.out.println("mail="+mail);
+
+        String password = request.getParameter ("password");
+        System.out.println("password="+password);
+
+        Boolean sexe=Boolean.parseBoolean(request.getParameter("sexe"));
+        System.out.println("sexe="+sexe);
+        
+        Client nouveauClient=new Client(password, prenom, nom, adresse,tel, sexe,mail, dateDeNaissance);//dateDeNaissance);
+        System.out.println(nouveauClient);
+
+        Service service=new Service();
+        
+        Long resInscription=service.inscrireClient(nouveauClient);
+        System.out.println(resInscription);
+        if(resInscription!=null){
+            request.setAttribute("inscription",true);
+        }else{
+            request.setAttribute("inscription",false);
+        }
+        
+    }
+    
+}
