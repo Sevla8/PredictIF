34f4e0da9a15f1c3b0f8c09a23748d08da267fa7
Algourdin Benoit <benoit.algourdin@insa-lyon.fr>
2021-05-17 15:45:04 +0000

    Upload New File

---
+++ PredictIF-IHM/src/main/java/web/serialisation/InscrireClientSerialisation.java
@@ -0,0 +1,40 @@
+
+package serialisation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Benoit
+ */
+public class InscrireClientSerialisation extends Serialisation {
+
+   
+    @Override
+    public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JsonObject container=new JsonObject();
+
+        Boolean inscription=(Boolean) request.getAttribute("inscription");
+        
+        container.addProperty("inscription",inscription);  
+        
+        response.setContentType("application/json;charset=UTF -8");
+        PrintWriter  out = this.getWriter (response);
+        Gson  gson = new  GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        gson.toJson(container , out);
+        out.close();
+    }
+    
+}
