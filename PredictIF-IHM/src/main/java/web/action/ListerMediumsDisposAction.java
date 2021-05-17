6378685d2c4e09159626ec1d49d6ebcd82e1d99a
Prevot Jade <jade.prevot@insa-lyon.fr>
2021-05-17 15:58:32 +0000

    Upload New File

---
+++ PredictIF-IHM/src/main/java/web/action/ListerMediumsDisposAction.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.dasi.ihm.web.action;
+
+import fr.insalyon.dasi.metier.modele.Medium;
+import fr.insalyon.dasi.metier.service.Service;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author jadep
+ */
+public class ListerMediumsDisposAction extends Action{
+    
+    @Override
+    public void executer(HttpServletRequest request){
+        
+        Service service = new Service();
+        
+        //List<Medium> listeMediums = service.listerMediums();
+        
+        List<Medium> listeMediumsDispos = service.listerMediumsDisponibles(3);
+        
+        //System.out.println("liste des mediums = " + mediums);
+        request.setAttribute("mediums-dispos", listeMediumsDispos);
+    }
+}
