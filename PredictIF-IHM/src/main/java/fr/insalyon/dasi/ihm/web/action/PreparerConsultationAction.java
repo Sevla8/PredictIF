977310f9bcae4512761be927e7c94b885cd09994
de Lambertye Gregoire <gregoire.de-lambertye@insa-lyon.fr>
2021-05-18 20:02:40 +0000

    Upload New File

---
+++ PredictIF-IHM/src/main/java/fr/insalyon/dasi/ihm/web/action/PreparerConsultationAction.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.action;
+
+import fr.insalyon.dasi.metier.modele.Consultation;
+import fr.insalyon.dasi.metier.modele.Employe;
+import fr.insalyon.dasi.metier.service.Service;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+public class PreparerConsultationAction extends Action{
+    @Override
+    public void executer(HttpServletRequest request){
+        HttpSession session = request.getSession(true); // Initialisation Session
+        if(session.getAttribute("id")!= null)
+        {
+            Service service = new Service();
+            Employe employe = service.trouverEmployeParId((Long)(session.getAttribute("id")));
+            Consultation consultation = service.obtenirConsultationAffectee(employe);
+            /**/
+            Long b = 64L;
+            consultation = service.trouverConsultationParId(b);
+            /***/
+            if(consultation != null)
+            {
+                List<Consultation> historique = service.obtenirHistorique(consultation.getClient());
+                request.setAttribute("ConsultationEnCour", true);
+                request.setAttribute("consultation", consultation);
+                request.setAttribute("historique", historique);  
+            }else{
+                request.setAttribute("ConsultationEnCour", false);
+            }
+            
+        }
+    }
+}
+
+
