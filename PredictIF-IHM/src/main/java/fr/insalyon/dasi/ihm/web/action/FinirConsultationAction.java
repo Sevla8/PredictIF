bd2e81e5a5c2e3e9cacf27b493313c7138785728
de Lambertye Gregoire <gregoire.de-lambertye@insa-lyon.fr>
2021-05-18 20:02:21 +0000

    Upload New File

---
+++ PredictIF-IHM/src/main/java/fr/insalyon/dasi/ihm/web/action/FinirConsultationAction.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.action;
+
+import fr.insalyon.dasi.metier.modele.Consultation;
+import fr.insalyon.dasi.metier.service.Service;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+public class FinirConsultationAction extends Action{
+    @Override
+    public void executer(HttpServletRequest request){
+        HttpSession session = request.getSession(true); // Initialisation Session
+        if(session.getAttribute("id")!= null)
+        {
+            Service service = new Service();
+            String idConsultation = request.getParameter("idConsultation");
+            Long idConsult = Long. parseLong(idConsultation);
+            Consultation consultation = service.trouverConsultationParId(idConsult);
+            consultation = service.finirConsultation(consultation);
+            
+            if(consultation != null){
+                request.setAttribute("fin", true);
+            }else{
+                request.setAttribute("fin", false);
+            }
+        }
+    }
+}
