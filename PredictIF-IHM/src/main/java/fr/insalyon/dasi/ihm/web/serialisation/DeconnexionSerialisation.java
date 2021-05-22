ce638640d1a75f77a0ba568fe3fb61363911e25d
Algourdin Benoit <benoit.algourdin@insa-lyon.fr>
2021-05-22 14:17:43 +0000

    Upload New File

---
+++ PredictIF-IHM/src/main/java/fr/insalyon/dasi/ihm/web/serialisation/DeconnexionSerialisation.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serialisation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import metier.modele.Client;
+
+
+/**
+ *
+ * @author Benoit
+ */
+public class DeconnexionSerialisation extends Serialisation {
+
+   
+    @Override
+    public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JsonObject container=new JsonObject();
+
+        Boolean connexion=(Boolean) request.getAttribute("connexion");
+                
+        container.addProperty("connecte",connexion);  
+        
+        response.setContentType("application/json;charset=UTF -8");
+        PrintWriter  out = this.getWriter (response);
+        Gson  gson = new  GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        gson.toJson(container , out);
+        out.close();
+    }
+    
+}
