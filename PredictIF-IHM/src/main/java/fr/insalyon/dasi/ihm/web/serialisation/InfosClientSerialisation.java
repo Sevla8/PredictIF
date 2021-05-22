85190ac0aca56046f4e21eae9073d25a5b3bd7d9
Algourdin Benoit <benoit.algourdin@insa-lyon.fr>
2021-05-22 14:25:08 +0000

    Upload New File

---
+++ PredictIF-IHM/src/main/java/fr/insalyon/dasi/ihm/web/serialisation/InfosClientSerialisation.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serialisation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import metier.modele.Client;
+
+
+/**
+ *
+ * @author Benoit
+ */
+public class InfosClientSerialisation extends Serialisation {
+
+   
+    @Override
+    public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        JsonObject container=new JsonObject();
+
+        Boolean connexion=(Boolean) request.getAttribute("connexion");
+        
+        Client client=(Client) request.getAttribute("client");
+        
+        container.addProperty("connecte",connexion);        
+        
+        JsonObject infosClients=new JsonObject();//Création d'un objet json pour un client
+
+        //Ajout de propriété à ce nouvel objet json
+        infosClients.addProperty("nom",client.getNom());
+        infosClients.addProperty("prenom",client.getPrenom());
+        SimpleDateFormat formater=new SimpleDateFormat("dd/MM/yy");
+        infosClients.addProperty("date",formater.format(client.getDateDeNaissance()));
+
+            
+        container.add("client", infosClients);
+        
+        response.setContentType("application/json;charset=UTF -8");
+        PrintWriter  out = this.getWriter (response);
+        Gson  gson = new  GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        gson.toJson(container , out);
+        out.close();
+    }
+    
+}
